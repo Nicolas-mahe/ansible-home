af7ef2c8bd0eea924ee1249ffc5df96b9a95e2c3
Nicolas Mahé <nicolas.mahe33@gmail.com>
2025-05-18 15:34:41 +0000

    entrypoint | manage ansible verbosity in docker vars

--- packages/entrypoint.sh
+++ packages/entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# Ansible verbosity management
+VERBOSITY=""
+if [ ! -z "$ANSIBLE_VERBOSITY" ]; then
+    case "$ANSIBLE_VERBOSITY" in
+        "0") VERBOSITY="" ;;
+        "1") VERBOSITY="-v" ;;
+        "2") VERBOSITY="-vv" ;;
+        "3") VERBOSITY="-vvv" ;;
+        "4") VERBOSITY="-vvvv" ;;
+        "5") VERBOSITY="-vvvvv" ;;
+    esac
+    echo "Ansible verbosity is level $ANSIBLE_VERBOSITY"
+else
+    echo "Ansible verbosity is not set by user, use default value: '1'"
+    VERBOSITY="-v"
+fi
+
+ansible-playbook playbooks/UpdateServer.yaml $VERBOSITY
+
 tail -f /dev/null
